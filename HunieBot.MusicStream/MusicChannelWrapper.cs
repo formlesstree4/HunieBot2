ea49ab05a05581a1d538d37d72254090577a879f
Adam Sears <asears2@live.com>
2016-05-06 23:34:04 -0500

    An incomplete MusicStream addition.

---
+++ HunieBot.MusicStream/MusicChannelWrapper.cs
@@ -0,0 +1,153 @@
+﻿using Discord.Audio;
+using NAudio.Wave;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HunieBot.MusicStream
+{
+
+    /// <summary>
+    ///     A class that wraps around a <see cref="IAudioClient"/> instance to provide streaming services.
+    /// </summary>
+    public sealed class MusicChannelWrapper : IDisposable
+    {
+        private bool _isPlaying = false;
+        private bool _canPlay = true;
+        private bool _disposedValue = false; // To detect redundant calls
+        private readonly int _channels;
+        private readonly IAudioClient _voiceChannel;
+
+
+
+        /// <summary>
+        ///     Raised when <see cref="MusicChannelWrapper"/> has finished playing a song.
+        /// </summary>
+        public EventHandler Finished;
+
+
+
+        /// <summary>
+        ///     Gets the <see cref="IAudioClient"/> that either is having music streamed to it or will be streaming.
+        /// </summary>
+        public IAudioClient Channel => _voiceChannel;
+
+        /// <summary>
+        ///     Indicates whether or not <see cref="MusicChannelWrapper"/> is currently playing.
+        /// </summary>
+        public bool IsPlaying => _isPlaying;
+
+        /// <summary>
+        ///     Gets or sets whether or not this <see cref="MusicChannelWrapper"/> supports automatically playing songs.
+        /// </summary>
+        /// <remarks>
+        ///     this is a specific interation between <see cref="MusicChannelWrapper"/> and <see cref="MusicStream"/>. If true, <see cref="MusicStream"/> will pop off another, random song to play.
+        /// </remarks>
+        public bool AutoPlay { get; set; }
+
+        /// <summary>
+        ///     Gets the name of the file that is currently playing.
+        /// </summary>
+        public string CurrentlyPlaying { get; private set; }
+
+
+
+        /// <summary>
+        ///     Creates a new instance of the <see cref="MusicChannelWrapper"/>.
+        /// </summary>
+        /// <param name="client"><see cref="IAudioClient"/></param>
+        /// <param name="audioChannels">The supported number of channels</param>
+        public MusicChannelWrapper(IAudioClient client, int audioChannels)
+        {
+            _voiceChannel = client;
+            _channels = audioChannels;
+        }
+
+        /// <summary>
+        ///     Plays a file from the file system.
+        /// </summary>
+        /// <param name="file"></param>
+        public void Play(string file)
+        {
+            _canPlay = true;
+            CurrentlyPlaying = Path.GetFileNameWithoutExtension(file);
+            Task.Run(() =>
+            {
+                var OutFormat = new WaveFormat(48000, 16, _channels);
+                using (var fileStream = File.Open(file, FileMode.Open, FileAccess.Read, FileShare.Read))
+                using (var MP3Reader = new Mp3FileReader(fileStream))
+                using (var resampler = new MediaFoundationResampler(MP3Reader, OutFormat))
+                {
+                    resampler.ResamplerQuality = 60; // Set the quality of the resampler to 60, the highest quality
+                    int blockSize = OutFormat.AverageBytesPerSecond / 50; // Establish the size of our AudioBuffer
+                    byte[] buffer = new byte[blockSize];
+                    int byteCount;
+                    _isPlaying = true;
+                    while (((byteCount = resampler.Read(buffer, 0, blockSize)) > 0) & _canPlay) // Read audio into our buffer, and keep a loop open while data is present
+                    {
+                        if (byteCount < blockSize)
+                        {
+                            // Incomplete Frame
+                            for (int i = byteCount; i < blockSize; i++)
+                                buffer[i] = 0;
+                        }
+                        try
+                        {
+                            _voiceChannel.Send(buffer, 0, blockSize);
+                        }
+                        catch (OperationCanceledException)
+                        {
+                            _canPlay = false;
+                        }
+                    }
+                    if(_canPlay) Finished?.Invoke(this, EventArgs.Empty);
+                    _isPlaying = false;
+                }
+            });
+        }
+
+        /// <summary>
+        ///     Stops the streaming.
+        /// </summary>
+        public void Stop()
+        {
+            _canPlay = false;
+        }
+
+        #region IDisposable Support
+
+        void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                    _voiceChannel.Channel.LeaveAudio();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                _disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~MusicChannelWrapper() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+
+    }
+}
---
+++ HunieBot.MusicStream/MusicStream.cs
@@ -0,0 +1,191 @@
+﻿using Discord;
+using Discord.Audio;
+using HunieBot.Host.Attributes;
+using HunieBot.Host.Enumerations;
+using HunieBot.Host.Interfaces;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using YoutubeExtractor;
+
+namespace HunieBot.MusicStream
+{
+
+    [HunieBot("Music Streaming Service")]
+    public sealed class MusicStream
+    {
+        private readonly Random _shuffle;
+        private readonly AudioService _audioSevice;
+        private readonly ConcurrentDictionary<ulong, MusicChannelWrapper> _voiceChannels;
+        private List<string> _playlistSource;
+        private ConcurrentQueue<string> _playlist;
+
+
+        public MusicStream(DiscordClient discordClient)
+        {
+            discordClient.UsingAudio(x =>
+            {
+                x.Mode = AudioMode.Outgoing;
+            });
+            _audioSevice = discordClient.GetService<AudioService>();
+            _voiceChannels = new ConcurrentDictionary<ulong, MusicChannelWrapper>();
+            _playlistSource = new List<string>();
+            _playlist = new ConcurrentQueue<string>();
+            _shuffle = new SecureRandom();
+        }
+
+
+        [HandleCommand(CommandEvent.MessageReceived | CommandEvent.CommandReceived, UserPermissions.Moderator, false, "jvc")]
+        public async Task JoinVoiceChannelOnServer(IHunieCommand command)
+        {
+            Channel c;
+            MusicChannelWrapper wrapper;
+
+            // Let's find the channel.
+            c = command.Server.FindChannels(command.Parameters[0]).FirstOrDefault();
+            if (c == null) return;
+
+            if(_voiceChannels.TryGetValue(command.Server.Id, out wrapper))
+            {
+                await command.User.SendMessage($"Moving to Voice Channel {c.Name}");
+                await wrapper.Channel.Join(c);
+                await command.User.SendMessage($"Moved to Voice Channel {c.Name}");
+            }
+            else
+            {
+                await command.User.SendMessage($"Joining Voice Channel {c.Name}");
+                wrapper = new MusicChannelWrapper(await _audioSevice.Join(c), _audioSevice.Config.Channels);
+                wrapper.AutoPlay = true;
+                wrapper.Finished += (sender, args) =>
+                {
+                    if (!wrapper.AutoPlay) return;
+                    wrapper.Play(GetNextSong());
+                };
+                await command.User.SendMessage($"Joining Voice Channel {c.Name}");
+                _voiceChannels.TryAdd(command.Server.Id, wrapper);
+            }
+        }
+
+        [HandleCommand(CommandEvent.MessageReceived | CommandEvent.CommandReceived, UserPermissions.Moderator, false, "qvc")]
+        public async Task QuitVoiceChannelOnServer(IHunieCommand command)
+        {
+            MusicChannelWrapper wrapper;
+            if (_voiceChannels.TryRemove(command.Server.Id, out wrapper))
+            {
+                await command.User.SendMessage($"Leaving Voice Channel {wrapper.Channel.Channel.Name}");
+                wrapper.Dispose();
+            }
+        }
+
+        [HandleCommand(CommandEvent.MessageReceived | CommandEvent.CommandReceived, UserPermissions.Moderator, false, "sps")]
+        public async Task SetPlaylistSourceDirectory(IHunieCommand command)
+        {
+            _playlistSource.Clear();
+            await command.User.SendMessage($"Populating the internal queue with all MP3 files in directory {command.Parameters[0]}. Depending on the size of the directory, this could take some time...");
+            _playlistSource.AddRange(Directory.EnumerateFiles(string.Join(" ", command.Parameters), "*.mp3", SearchOption.AllDirectories));
+            await command.User.SendMessage($"Finished! There are {_playlistSource.Count} items in the queue.");
+            ResetQueue();
+        }
+
+        [HandleCommand(CommandEvent.MessageReceived | CommandEvent.CommandReceived, UserPermissions.Moderator, false, "reloadqueue")]
+        public async Task ReloadPlaylist(IHunieCommand command)
+        {
+            ResetQueue();
+        }
+
+        [HandleCommand(CommandEvent.MessageReceived | CommandEvent.CommandReceived, UserPermissions.Moderator, false, "playing")]
+        public async Task NowPlayingOnServer(IHunieCommand command)
+        {
+            MusicChannelWrapper wrapper;
+            if (_voiceChannels.TryRemove(command.Server.Id, out wrapper))
+            {
+                await command.User.SendMessage($"Current Song: {wrapper.CurrentlyPlaying}");
+            }
+        }
+
+        [HandleCommand(CommandEvent.MessageReceived | CommandEvent.CommandReceived, UserPermissions.Moderator, false, "start")]
+        public async Task StartMusic(IHunieCommand command)
+        {
+            MusicChannelWrapper wrapper;
+            if (!_voiceChannels.TryGetValue(command.Server.Id, out wrapper)) return;
+            if (wrapper.IsPlaying) return;
+            wrapper.Play(GetNextSong());
+        }
+
+        [HandleCommand(CommandEvent.MessageReceived | CommandEvent.CommandReceived, UserPermissions.Moderator, false, "stop")]
+        public async Task StopMusic(IHunieCommand command)
+        {
+            MusicChannelWrapper wrapper;
+            if (!_voiceChannels.TryGetValue(command.Server.Id, out wrapper)) return;
+            if (!wrapper.IsPlaying) return;
+            wrapper.Stop();
+        }
+
+        [HandleCommand(CommandEvent.MessageReceived | CommandEvent.CommandReceived, UserPermissions.Moderator, false, "next")]
+        public async Task NextSong(IHunieCommand command)
+        {
+            MusicChannelWrapper wrapper;
+            if (!_voiceChannels.TryGetValue(command.Server.Id, out wrapper)) return;
+            if (!wrapper.IsPlaying) return;
+            var autoplay = wrapper.AutoPlay;
+            wrapper.AutoPlay = false;
+            wrapper.Stop();
+            wrapper.Play(GetNextSong());
+            wrapper.AutoPlay = autoplay;
+        }
+
+
+        private void ResetQueue()
+        {
+            _playlistSource.Shuffle(_shuffle);
+            _playlist = new ConcurrentQueue<string>(_playlistSource);
+        }
+        private string GetNextSong()
+        {
+            string next;
+            lock (_playlist)
+            {
+                if(_playlist.Count == 0)
+                {
+                    ResetQueue();
+                }
+                while (!_playlist.TryDequeue(out next)) ;
+            }
+            return next;
+        }
+        private string PeekNextInQueue()
+        {
+            string next;
+            lock (_playlist)
+            {
+                if (_playlist.Count == 0)
+                {
+                    ResetQueue();
+                }
+                while (!_playlist.TryPeek(out next)) ;
+            }
+            return next;
+        }
+
+    }
+
+    internal static class Extensions
+    {
+        public static void Shuffle<T>(this IList<T> list, Random rand)
+        {
+            // Random will 99% of the time be thread-safe. So fuckin' deal with it.
+            int n = list.Count;
+            for (int i = 0; i < n; i++)
+            {
+                // NextDouble returns a random number between 0 and 1.
+                int r = i + (int)(rand.NextDouble() * (n - i));
+                T t = list[r];
+                list[r] = list[i];
+                list[i] = t;
+            }
+        }
+    }
+}
