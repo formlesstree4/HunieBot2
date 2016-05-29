4bea1bd5759596c95b6ec0d9bf23da18ae9837da
@mariocatch <mariocatch@gmail.com>
2016-05-28 20:46:08 -0400

    Add payload receiver for github webhook

--- HunieBot2.Console/GitHubReceiver.cs
+++ HunieBot2.Console/GitHubReceiver.cs
@@ -1,17 +1,26 @@
 ﻿using Microsoft.AspNet.WebHooks;
-using System.Web.Http;
 using System.Threading.Tasks;
 using System.Linq;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace HunieBot2.Console
 {
     public class GitHubReceiver : WebHookHandler
     {
+        public GitHubReceiver()
+        {
+            this.Receiver = GitHubWebHookReceiver.ReceiverName;
+        }
+
         public override Task ExecuteAsync(string receiver, WebHookHandlerContext context)
         {
-            string action = context.Actions.First();
             JObject data = context.GetDataOrDefault<JObject>();
+            var result = JsonConvert.DeserializeObject<GitHubPayload>(data.ToString());
+            System.Console.WriteLine(result);
+            string action = context.Actions.First();
+
+
             return Task.FromResult(true);
         }
     }
