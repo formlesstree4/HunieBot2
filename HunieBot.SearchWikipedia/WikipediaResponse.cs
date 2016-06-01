37939490add5caabe4d490d073982539d3305557
@mariocatch <mariocatch@gmail.com>
2016-06-01 19:16:46 -0400

    Add .wiki <query> command implementation which queries the wikipedia api

---
+++ HunieBot.SearchWikipedia/WikipediaResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace HunieBot.WikipediaSearch
+{
+    internal class WikipediaResponse
+    {
+        public WikipediaQuery Query { get; set; }
+    }
+
+    internal class WikipediaQuery
+    {
+        public Dictionary<string, WikipediaPage> Pages { get; set; }
+    }
+
+    internal class WikipediaPage
+    {
+        public string Extract { get; set; }
+        public string Title { get; set; }
+    }
+}
