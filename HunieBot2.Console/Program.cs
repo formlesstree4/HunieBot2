507ac959cbb0e1582b78192c066616bac447a0ef
mariocatch <mariocatch@gmail.com>
2016-06-01 14:31:26 -0400

    Update branch from fork

--- HunieBot2.Console/Program.cs
+++ HunieBot2.Console/Program.cs
@@ -1,7 +1,4 @@
 ﻿#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-using Microsoft.Owin.Hosting;
-using System.Configuration;
-
 namespace HunieBot2.Console
 {
     class Program
@@ -10,15 +7,10 @@ namespace HunieBot2.Console
         {
             using (var b = new HunieBot.Host.HunieHost())
             {
-                var baseAddress = "http://localhost:1337";
-                using (WebApp.Start<Startup>(baseAddress))
-                {
-                    System.Console.WriteLine("WebApi started at: {0}", baseAddress);
-                    b.Configuration.DiscordToken = ConfigurationManager.AppSettings["DiscordToken"];
-                    b.Start().Wait();
-                    System.Console.ReadLine();
-                    b.Stop();
-                }
+                // b.Configuration.DiscordToken = "<token here>";
+                b.Start().Wait();
+                System.Console.ReadLine();
+                b.Stop();
             }
         }
     }
