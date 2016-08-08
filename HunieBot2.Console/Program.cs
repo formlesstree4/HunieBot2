f9cfe4db41973b537c50dd2d403471b171aa7e11
xeronik <xeronik@emulate.cc>
2016-08-08 02:15:42 +0200

    Adding Setup

--- HunieBot2.Console/Program.cs
+++ HunieBot2.Console/Program.cs
@@ -1,17 +1,35 @@
-﻿#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+﻿using System;
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 namespace HunieBot2.Console
 {
     class Program
     {
-        static void Main(string[] args)
+        public const string ConfigurationFile = "conf.json";
+
+        static void Main(string[] args) => new Program().Start();
+
+        public void Start()
         {
-            using (var honieHost = new HunieBot.Host.HunieHost())
+            using (var hunieHost = new HunieBot.Host.HunieHost())
             {
-                honieHost.Configuration.DiscordToken = "MjExNjkzNjkyNjQ4NTU0NDk3.CohCqQ.3e_F8DkpTqtHenCpHEhuaCM_TNA";
-                honieHost.Configuration.Game = "I can finally have my own game!";
-                honieHost.Start().Wait();
+                hunieHost.Configuration.Load(ConfigurationFile);
+                if (hunieHost.Configuration.DiscordToken == string.Empty)
+                {
+                    System.Console.WriteLine("Do you want to set up huniebot? Y/n");
+                    if (System.Console.ReadLine().ToLower() != "y")
+                        return;
+                    System.Console.WriteLine("Please enter your token below: ");
+                    hunieHost.Configuration.DiscordToken = System.Console.ReadLine();
+                    System.Console.WriteLine("Please enter your title: ");
+                    hunieHost.Configuration.Game = System.Console.ReadLine();
+                    System.Console.WriteLine("Please enter your preferred prefix key: ");
+                    hunieHost.Configuration.CommandCharacter = Convert.ToChar(System.Console.ReadLine());
+                    hunieHost.Configuration.Save(ConfigurationFile);
+                }
+                hunieHost.Start().Wait();
                 System.Console.ReadLine();
-                honieHost.Stop();
+                hunieHost.Stop();
             }
         }
     }
