c5845eaabc9aeb2af55b52b0d14456b5dff87907
darkkal <darkkal999@gmail.com>
2016-10-15 12:09:26 -0500

    Copypastas are now separated by server. This is the next step in moving from json to a sqlite database.

--- HunieBot.CopyPasta/CopyPasta.cs
+++ HunieBot.CopyPasta/CopyPasta.cs
@@ -10,10 +10,11 @@ using Newtonsoft.Json;
 
 namespace HunieBot.CopyPasta
 {
+    // todo: move to sqlite database to maintain convention of persisting data storage to a sqlite database handled by HunieBot
     [HunieBot(nameof(CopyPasta))]
     public class CopyPasta
     {
-        private Dictionary<string, string> _copyPastas;
+        private IEnumerable<Pasta> _pastaData;
 
         private const char PastaEchoCommandToken = '~';
         private const string SavedPastasFileName = "copypasta.json";
@@ -27,19 +28,33 @@ namespace HunieBot.CopyPasta
                                                   "\t`.pasta|.meme [pasta_name]` - removes copypasta with `pasta_name`\n" +
                                                   "\t`.pasta|.meme -list` - PMs you the list of currently saved copypastas\n" +
                                                   "To make me echo a copypasta, use `~pasta_name`";
-                                                    
 
-        private Dictionary<string, string> CopyPastas
+        private IEnumerable<Pasta> PastaData
         {
             get
             {
-                if (_copyPastas != null) return _copyPastas;
+                if (_pastaData != null) return _pastaData;
 
-                LoadPastas();
+                var filePath = Path.Combine(_workingDirectory, SavedPastasFileName);
 
-                return _copyPastas;
+                if (!File.Exists(filePath)) File.Create(filePath);
+                var fileText = File.ReadAllText(filePath);
+                if (string.IsNullOrWhiteSpace(fileText))
+                    return new List<Pasta>();
+                
+                _pastaData = JsonConvert.DeserializeObject<IEnumerable<Pasta>>(fileText);
+
+                return _pastaData;
+            }
+            set
+            {
+                _pastaData = value;
+                var filePath = Path.Combine(_workingDirectory, SavedPastasFileName);
+                var serializedPastas = JsonConvert.SerializeObject(_pastaData);
+                File.WriteAllText(filePath, serializedPastas);
             }
         }
+
 
         [HandleEvent(CommandEvent.MessageReceived | CommandEvent.AnyMessageReceived)]
         public async Task HandlePastaEcho(IHunieMessage message, ILogging logger)
@@ -47,19 +62,23 @@ namespace HunieBot.CopyPasta
 
             try
             {
+                if (message.User.IsBot)
+                    return;
                 if (message.Message.Text.Length <= 1)
                     return;
                 if (message.Message.Text[0] != PastaEchoCommandToken)
                     return;
 
                 var pastaName = message.Message.Text.Substring(1);
-                if (!CopyPastas.ContainsKey(pastaName))
-                {
+
+                var pasta = PastaData.FirstOrDefault(p => p.ServerId == message.Server.Id && p.PastaName == pastaName);
+                if (string.IsNullOrWhiteSpace(pasta?.PastaContent))
+                { 
                     await message.Channel.SendMessage($"`{pastaName}` does not exist!");
                     return;
                 }
 
-                await message.Channel.SendMessage(CopyPastas[pastaName]);
+                await message.Channel.SendMessage(pasta.PastaContent);
             }
             catch (Exception e)
             {
@@ -81,7 +100,11 @@ namespace HunieBot.CopyPasta
                                 $"```{nameof(CopyPasta)}: -list"
                             };
 
-                    messageLines.AddRange(CopyPastas.Keys);
+                    var pastaList = from pasta in PastaData
+                        where pasta.ServerId == command.Server.Id
+                        select pasta.PastaContent;
+                    
+                    messageLines.AddRange(pastaList);
                     messageLines.Add("```");
 
                     await command.User.SendMessage(string.Join("\n", messageLines));
@@ -103,88 +126,54 @@ namespace HunieBot.CopyPasta
                     }
 
                     var pastaName = command.ParametersArray[0];
-                    if (!CopyPastas.ContainsKey(pastaName))
+                    var pasta = PastaData.FirstOrDefault(p => p.ServerId == command.Server.Id && p.PastaName == pastaName);
+                    if (string.IsNullOrWhiteSpace(pasta?.PastaContent))
                     {
                         await command.Channel.SendMessage($"`{pastaName}` does not exist!");
                         return;
                     }
 
-                    CopyPastas.Remove(pastaName);
-                    SavePastas();
+                    var pastaDataList = (List<Pasta>) PastaData;
+                    pastaDataList.Remove(pasta);
+                    PastaData = pastaDataList;
+
                     await command.Channel.SendMessage($"`{pastaName}` removed!");
                 }
                 else
                 {
+                    if (command.Server == null)
+                    {
+                        await command.Channel.SendMessage(
+                            $"{command.Command.ToUpperInvariant()}s must be added from within a server, not through PM.");
+                        return;
+                    }
+
+
                     var pastaName = command.ParametersArray[0];
-                    var pasta = command.Message.Text.Replace($".{command.Command} {pastaName}", "");
+                    var pastaContent = command.Message.Text.Replace($".{command.Command} {pastaName}", "");
+                    var pasta = new Pasta
+                    {
+                        ServerId = command.Server.Id,
+                        PastaName = pastaName,
+                        PastaContent = pastaContent
+                    };
 
-                    if (CopyPastas.ContainsKey(pasta))
-                        CopyPastas[pastaName] = pasta;
-                    else
-                        CopyPastas.Add(pastaName, pasta);
+                    if (PastaData.Any(p => p.ServerId == pasta.ServerId && p.PastaName == pasta.PastaName))
+                    {
+                        await
+                        command.Channel.SendMessage(
+                            $"{command.Command} already exists!");
+                        return;
+                    }
 
-                    SavePastas();
+                    var pastaDataList = (List<Pasta>)PastaData;
+                    pastaDataList.Add(pasta);
+                    PastaData = pastaDataList;
+
                     await
                         command.Channel.SendMessage(
                             $"{command.Command} saved! type `{PastaEchoCommandToken}{pastaName}` to echo it!");
                 }
-
-
-                //var option = command.Parameters.Keys.FirstOrDefault();
-
-                //switch (option)
-                //{
-                //    case "make":
-                //    {
-                //        var pastaName = command.ParametersArray[1];
-                //        var pasta = command.Message.Text.Replace($".{command.Command} -make {pastaName}", "");
-                //        CopyPastas.Add(pastaName, pasta);
-                //        SavePastas();
-                //        await
-                //            command.Channel.SendMessage(
-                //                $"{command.Command} saved! type `.{command.Command} {pastaName}` to echo it!");
-                //        return;
-                //    }
-                //    case "list":
-                //    {
-                //        var messageLines = new List<string>
-                //        {
-                //            $"```{nameof(CopyPasta)}: -list"
-                //        };
-
-                //        messageLines.AddRange(CopyPastas.Keys);
-                //        messageLines.Add("```");
-
-                //        await command.User.SendMessage(string.Join("\n", messageLines));
-                //        return;
-                //    }
-                //    case "remove":
-                //    {
-                //        var pastaName = command.ParametersArray[1];
-                //        if (!CopyPastas.ContainsKey(pastaName))
-                //        {
-                //            await command.User.SendMessage($"`{pastaName}` does not exist!");
-                //            return;
-                //        }
-
-                //        CopyPastas.Remove(pastaName);
-                //        SavePastas();
-                //        await command.Channel.SendMessage($"`{pastaName}` removed!");
-                //        return;
-                //    }
-                //    default:
-                //    {
-                //        if (CopyPastas.ContainsKey(command.ParametersArray[0]))
-                //        {
-                //            var pastaName = command.ParametersArray[0];
-                //            await command.Channel.SendMessage(CopyPastas[pastaName]);
-                //            return;
-                //        }
-
-                //        await command.Channel.SendMessage(HelpText);
-                //        return;
-                //    }
-                //}
             }
             catch (Exception e)
             {
@@ -193,26 +182,45 @@ namespace HunieBot.CopyPasta
             }
         }
 
-        private void LoadPastas()
+        //[HandleEvent(CommandEvent.MessageReceived | CommandEvent.AnyMessageReceived)]
+        //public async Task HandleMergeCommand(IHunieMessage message, ILogging logger)
+        //{
+        //    if (message.Message.Text != "<some randomly generated password>")
+        //        return;
+
+        //    var oldFilePath = Path.Combine(_workingDirectory, "copypasta_old.json");
+
+        //    var oldFileText = File.ReadAllText(oldFilePath);
+        //    PastaData = ConvertOldPastaToNew(oldFileText);
+        //}
+
+        /// <summary>
+        /// Helper function to convert an old copypasta.json file 
+        /// using a serialized Dictionary(string, string)
+        /// to the new json format using the new <see cref="Pasta"/> object
+        /// </summary>
+        /// <param name="oldPastaJson">the old copypasta.json file in string form</param>
+        /// <returns>the json string in the new format using <see cref="Pasta"/></returns>
+        private IEnumerable<Pasta> ConvertOldPastaToNew(string oldPastaJson)
         {
-            // todo: change this when we move to a different filestructure for modules, or leave it and keep it in %APPDATA%
-            var filePath = Path.Combine(_workingDirectory, SavedPastasFileName);
+            var serverIdList = new List<ulong> {  };
 
-            if (!File.Exists(filePath)) File.Create(filePath);
-            var fileText = File.ReadAllText(filePath);
-            _copyPastas = JsonConvert.DeserializeObject<Dictionary<string, string>>(fileText) ??
-                          new Dictionary<string, string>();
+            var oldPastas = JsonConvert.DeserializeObject<Dictionary<string, string>>(oldPastaJson);
+            return from serverId in serverIdList
+                from oldPasta in oldPastas
+                select new Pasta
+                {
+                    ServerId = serverId,
+                    PastaName = oldPasta.Key,
+                    PastaContent = oldPasta.Value
+                };
         }
+    }
 
-        private void SavePastas()
-        {
-            // todo: change this when we move to a different filestructure for modules, or leave it and keep it in %APPDATA%
-            var filePath = Path.Combine(_workingDirectory, SavedPastasFileName);
-
-            // probably not necessary since File.WriteAllText() overwrites existing files
-            // if (!File.Exists(filePath)) File.Create(filePath);
-            var serializedPastas = JsonConvert.SerializeObject(CopyPastas);
-            File.WriteAllText(filePath, serializedPastas);
-        }
+    public class Pasta
+    {
+        public ulong ServerId { get; set; }
+        public string PastaName { get; set; }
+        public string PastaContent { get; set; }
     }
 }
